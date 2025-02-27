65758316256479f2afa9cdd6196cd468e125f637
LeonardoRoro2 <23041078@itdurango.edu.mx>
2025-02-26 18:17:05 -0600

    Update Practica 1.py
    Se añadio el método de centros al cuadrado y se modificó el código en general para mejorar la presentación

--- Unidad 2/Practica 1.py
+++ Unidad 2/Practica 1.py
@@ -1,16 +1,48 @@
-# Semilla Inicial
-semilla = 2005
-n = 4
-iteraciones = 100
-resultados = []
+# Se eleva al cuadrado la semilla, se calcula la nueva semilla y se devuelve el nuevo valor de la semilla
+def Calcular_Semilla_MC(semilla, n_digitos):
+    # Elevar la semilla al cuadrado
+    cuadrado = str(semilla ** 2).zfill(2*n_digitos) # Agregar los espacios necesarios
+    # Extraer los digitos centrales
+    centro = cuadrado[len(cuadrado)//2 - n_digitos//2 : len(cuadrado)//2 + n_digitos//2]
+    return int(centro)
+    
+# Se realiza el producto de las dos semillas, se calculan la nueva semilla y se devuelven los nuevos valores de las semillas
+def Calcular_Semilla_CC(semilla, semilla2, n_digitos):
+    # Realizar el producto de las dos semillas
+    producto = str(semilla * semilla2).zfill(2*n_digitos) # Agregar los espacios necesarios
+    # Extraer los digitos centrales
+    centro = producto[len(producto)//2 - n_digitos//2 : len(producto)//2 + n_digitos//2]
+    return semilla2, int(centro)
+    
+# Numeros aleatorios calculados por el metodo de medios al cuadrado
+def medios_al_cuadrado(semilla,n_digitos,iteraciones):
+    resultados = []
+    for _ in range(iteraciones):
+        semilla = Calcular_Semilla_MC(semilla, n_digitos)
+        resultados.append(semilla)
+    return resultados
+# Numeros aleatorios calculados por el metodo de centros al cuadrado
+def centros_al_cuadrado(semilla,semilla2,n_digitos,iteraciones):
+    resultados = []
+    for _ in range(iteraciones):
+        semilla, semilla2 = Calcular_Semilla_CC(semilla,semilla2, n_digitos)
+        resultados.append(semilla2)
+    return resultados
 
-for _ in range(iteraciones):
-  # Elevar la semilla al cuadrado
-  cuadrado = str(semilla ** 2).zfill(2*n) # Agregar los espacios necesarios
-  # Extraer los digitos centrales
-  inicio = (len(cuadrado) - n) // 2
-  semilla = int(cuadrado[inicio:inicio + n])
-  resultados.append(semilla)
+# Semilla Inicial
+semilla = 2111
+n_digitos = 4
+#semilla2 = Calcular_Semilla_MC(semilla, n_digitos)
+semilla2 = 1999
+iteraciones = 100
+
 # Imprimir los resultados
-for i, numero in enumerate(resultados, 1):
+# Medios al cuadrado
+print("Numeros aleatorios calculados por el metodo de medios al cuadrado")
+for i, numero in enumerate(medios_al_cuadrado(semilla,n_digitos,iteraciones), 1):
+  print(f"Numero Aleatorio {i}: {numero}")
+print("")
+# Centros al cuadrado
+print("Numeros aleatorios calculados por el metodo de centros al cuadrado")
+for i, numero in enumerate(centros_al_cuadrado(semilla,semilla2,n_digitos,iteraciones), 1):
   print(f"Numero Aleatorio {i}: {numero}")
